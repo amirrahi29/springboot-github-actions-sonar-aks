6858ea3b37f0bd5cd8a3dac4b912fada95cb7c00
Amir Rahi <amirrahi29@gmail.com>
2026-03-30 04:22:40 +0530

    some changes

--- src/main/java/com/chat/app/ChatAppApplication.java
+++ src/main/java/com/chat/app/ChatAppApplication.java
@@ -3,6 +3,18 @@ package com.chat.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+
+
+
+
+
+
+
+
+
 @SpringBootApplication
 public class ChatAppApplication {
 	public static void main(String[] args) {
